4c55a85314fff1d2231380e59fe81c9ca3628636
Marek Tomka <marektomka@DESKTOP-F8IDB77.localdomain>
2020-12-15 14:30:11 +0100

    zapocet2

---
+++ postfix.py
@@ -0,0 +1,85 @@
+
+
+
+def eval_expr(s,d={}):
+
+    s=s.split()
+    n=len(s)
+    zasobnik=[]
+
+    for i in range(n):
+
+        if s[i] in d:
+        
+            zasobnik.append(int(d[s[i]]))
+
+        if s[i].isdigit():
+
+            zasobnik.append(int(s[i]))
+
+        elif s[i]=="+":
+            a=zasobnik.pop()
+            b=zasobnik.pop()
+            zasobnik.append(int(a)+int(b))
+
+        elif s[i]=="-":
+            a=zasobnik.pop()
+            b=zasobnik.pop()
+            zasobnik.append(int(a)-int(b))
+
+        elif s[i]=="*":
+            a=zasobnik.pop()
+            b=zasobnik.pop()
+            zasobnik.append(int(a)*int(b))
+
+        elif s[i]=="/":
+            a=zasobnik.pop()
+            b=zasobnik.pop()
+            zasobnik.append(int(a)/int(b))
+
+    return zasobnik.pop()
+
+
+def to_infix(s):
+    
+    s=s.split()
+    stack=[]
+    n=len(s)
+    
+    for i in range(n):
+
+        if s[i].isdigit():
+
+            stack.append(int(s[i]))
+
+        elif s[i]=="+":
+            a=stack.pop()
+            b=stack.pop()
+            string="( {} + {} )".format(a,b)
+            stack.append(string)
+
+        elif s[i]=="-":
+            a=stack.pop()          
+            b=stack.pop()
+            string="( {} - {} )".format(a,b)
+            stack.append(string)
+
+        elif s[i]=="*":
+            a=stack.pop()                                                                                           
+            b=stack.pop()                                                                                           
+            string="( {} * {} ) ".format(a,b)
+            stack.append(string)
+        
+        elif s[i]=="/":
+            a=stack.pop()                                                                                           
+            b=stack.pop()                                                                                           
+            string=" ( {} / {} ) ".format(a,b)
+            stack.append(string)
+
+    print(stack.pop())
+        
+
+
+
+
+
